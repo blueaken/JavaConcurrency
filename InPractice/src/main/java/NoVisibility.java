33627155596a20577dd975723e23a86bd1305b19
jianshen <jianshen@ebay.com>
2014-09-19 16:43:53 -0700

    update

---
+++ InPractice/src/main/java/NoVisibility.java
@@ -0,0 +1,21 @@
+/**
+ * @author jianshen
+ */
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+    private static class ReaderThread extends Thread {
+        public void run() {
+            while (!ready)
+                Thread.yield();
+            System.out.println(number);
+        }
+    }
+    public static void main(String[] args) {
+        for (int i=0; i<15; i++){
+            new ReaderThread().start();
+            number = 42;
+            ready = true;
+        }
+    }
+}
