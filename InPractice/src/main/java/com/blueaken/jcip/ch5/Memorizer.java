8a1b8a72506aa8fc664263dd23f8d3c9dbaea52d
BruceShen <blueaken@gmail.com>
2014-09-30 16:54:11 -1000

    final perfect version of Memorizer

---
+++ InPractice/src/main/java/com/blueaken/jcip/ch5/Memorizer.java
@@ -0,0 +1,48 @@
+package com.blueaken.jcip.ch5;
+
+import java.util.concurrent.*;
+
+/**
+ * Author: blueaken
+ * Date: 9/30/14 7:51 下午
+ */
+public class Memorizer<A, V> implements Computable<A, V> {
+    private final ConcurrentMap<A, Future<V>> cache
+            = new ConcurrentHashMap<A, Future<V>>();
+    private final Computable<A, V> c;
+    public Memorizer(Computable<A, V> c) { this.c = c; }
+    public V compute(final A arg) throws InterruptedException {
+        while (true) {
+            Future<V> f = cache.get(arg);
+            if (f == null) {
+                Callable<V> eval = new Callable<V>() {
+                    public V call() throws InterruptedException {
+                        return c.compute(arg);
+                    }
+                };
+                FutureTask<V> ft = new FutureTask<V>(eval);
+                f = cache.putIfAbsent(arg, ft);
+                if (f == null) { f = ft; ft.run(); }
+            }
+            try {
+                return f.get();
+            } catch (CancellationException e) {
+                cache.remove(arg, f);
+            } catch (ExecutionException e) {
+                throw launderThrowable(e.getCause());
+            }
+        }
+    }
+
+    /** If the Throwable is an Error, throw it; if it is a
+     * RuntimeException return it, otherwise throw IllegalStateException
+     */
+    public static RuntimeException launderThrowable(Throwable t) {
+        if (t instanceof RuntimeException)
+            return (RuntimeException) t;
+        else if (t instanceof Error)
+            throw (Error) t;
+        else
+            throw new IllegalStateException("Not unchecked", t);
+    }
+}
